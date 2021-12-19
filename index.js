cf83f2ab77b1b852ca905a1700c341d380678207
Anton <45075347+slpkbt@users.noreply.github.com>
2021-12-19 13:21:00 +0300

    Fixs + More power
    Увеличена скорость за счёт добавления тредов, фиксы под новые сообщения дискорда

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-process.on('uncaughtException', (err) => { }); // Ignore ECONNRESET and other request errors
+process.on('uncaughtException', (err) => { console.log(err) }); // Ignore ECONNRESET and other request errors, but we logs critical error on startup
 process.on('unhandledRejection', (err) => { }); // Ignore ECONNRESET and other request errors
 
 // Import modules
@@ -10,12 +10,17 @@ const fs = require(`fs`);
 // Options
 
 let glength = 16 // This means the number of characters in the gift code, it's usually 16, but discord can change that
-let intervalms = 0 // MS of interval (The smaller, the faster. Not less than zero.) 
+let intervalms = 1000 // MS of interval (The smaller, the faster. Not less than zero.) 
+let threads = 300 // How much intervals we should create
 let proxy = `proxy.txt` // List of proxies that generator will use
 let logerrors = false // Log errors (better turn it off)
+let logcustom = false // Log custom messages from discord api (ratelimit etc)
 const webhookurl = `` // Your webhook url to send logs right in discord
 
 if (!webhookurl) return console.log(`Please configure your webhook at 16th line!`)
+
+let unknown = 0
+let success = 0
 
 // Main functions
 
@@ -51,37 +56,44 @@ function start() { // Start function
     }
 
     console.log(`Loaded ${proxies.length} proxies, make sure that they're HTTP/S!`)
+    for(let i = 0; i < threads; i++) {
+        setInterval(() => {
 
-    setInterval(() => {
+            let code = g(glength);
+    
+            let takedproxy = proxies[Math.floor(Math.random() * proxies.length)] // We take a random proxy from our proxy file
+    
+            request({
+                url: `https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=true&with_subscription_plan=true`,
+                proxy: 'http://' + takedproxy
+            }, (err, res, body) => {
+    
+                total++
+                process.title = `giftbeast | ${total} requests | ${unknown} invalid | ${success} valid`
+    
+                if (err) return logerrors ? console.log(`[i] Error on ${takedproxy}`) : false
+    
+                let p = JSON.parse(body)
+    
+                if (p.message == `You are being rate limited.` || p.message == `The resource is being rate limited.`) {
+                    if(logcustom) return console.log(`Rate limit on ${takedproxy}`)
+                    return
+                }
+    
+                if (p.message == `Unknown Gift Code`) {
+                    unknown++
+                    console.clear()
+                    return console.log(`---------GIFTBEAST---------\n${unknown} invalid\n${success} valid\n${total} total`)
+                }
+    
+                console.log(`sending ${code} with message ${p.message}`)
+    
+                log(`NitroCode found!\nCode: ${code}`)
+    
+            })
+        }, intervalms);
+    }
 
-        let code = g(glength);
-
-        let takedproxy = proxies[Math.floor(Math.random() * proxies.length)] // We take a random proxy from our proxy file
-
-        request({
-            url: `https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=true&with_subscription_plan=true`,
-            proxy: 'http://' + takedproxy
-        }, (err, res, body) => {
-
-            total++
-            process.title = `giftbeast | ${total} total`
-
-            if (err) return logerrors ? console.log(`[i] Error on ${takedproxy}`) : false
-
-            let p = JSON.parse(body)
-
-            if (p.message == `You are being rate limited.`) {
-                return console.log(`Rate limit on ${takedproxy}`)
-            }
-
-            if (p.message == `Unknown Gift Code`) {
-                return console.log(`Unknown gift code on ${takedproxy}`)
-            }
-
-            log(`Potential code found!\nCode: ${code}`)
-
-        })
-    }, intervalms);
 
 }
 
